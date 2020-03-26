3e71c6b28edf7314a9a0aca5afbd341491e02ed2
Andrew Alumkal <Andrew.Alumkal@gmail.com>
2020-03-26 15:00:13 -0400

    AddGeneralSQLHealthTest

---
+++ src/SQLRoboFailover/Functions/Test-IsSQLServerHealthy.ps1
@@ -0,0 +1,55 @@
+Function Test-IsSQLServerHealthy {
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $ServerInstance,
+
+        [Parameter(Mandatory = $false)]
+        [Switch]$RunExtendedAGChecks
+    )
+
+
+    [bool]$IsSQLServerHealthy = 1
+
+    $UnHealthyDBs = @(Get-UnHealthySQLDatabases -ServerInstance $ServerInstance)
+
+    if ($UnHealthyDBs.Count -gt 0) {
+        Write-Verbose "Found unhealthy databases on the server"
+        $IsSQLServerHealthy = 0
+        return $IsSQLServerHealthy
+    }
+
+
+    $AGReplicas = @(Get-AvailabilityGroupsOnServer -ServerInstance $ServerInstance)
+
+    if ($AGReplicas.Count -gt 0) {
+
+        if ($AGReplicas | Where-Object -Property ReplicaHealth -ne "HEALTHY") {
+            Write-Verbose "Found replicas that are not in a healthy state"
+            $IsSQLServerHealthy = 0
+            return $IsSQLServerHealthy
+        }
+    
+        if ($AGReplicas | Where-Object -Property ReplicaConnectedState -ne "CONNECTED") {
+            Write-Verbose "Found replicas that are not in a connected state"
+            $IsSQLServerHealthy = 0
+            return $IsSQLServerHealthy
+        }
+    
+        #Check DB Level AG health
+        [bool]$TestAGDBState = 0
+        $TestAGDBState = Test-AllAGDatabasesOnServerHealthy -ServerInstance $ServerInstance -RunExtendedChecks:$RunExtendedAGChecks
+
+        if(!$TestAGDBState){
+            Write-Verbose "Found AG databases that are not in a healthy state"
+            $IsSQLServerHealthy = 0
+            return $IsSQLServerHealthy
+        }
+        
+
+    }
+    
+    return $IsSQLServerHealthy
+    
+}
